07e5b315517f817e8e61c9f4bee2870e399c0d5c
Ilya <ikozin@yandex.ru>
2016-09-27 19:01:38 +0300

    fix

--- test.py
+++ test.py
@@ -1,3 +1,115 @@
+#from tkinter import *
+#from tkinter import ttk
+#root = Tk()
+
+## Initialize our country "databases":
+##  - the list of country codes (a subset anyway)
+##  - a parallel list of country names, in the same order as the country codes
+##  - a hash table mapping country code to population<
+#countrycodes = ('ar', 'au', 'be', 'br', 'ca', 'cn', 'dk', 'fi', 'fr', 'gr', 'in', 'it', 'jp', 'mx', 'nl', 'no', 'es', 'se', 'ch')
+#countrynames = ('Argentina', 'Australia', 'Belgium', 'Brazil', 'Canada', 'China', 'Denmark', \
+#        'Finland', 'France', 'Greece', 'India', 'Italy', 'Japan', 'Mexico', 'Netherlands', 'Norway', 'Spain', \
+#        'Sweden', 'Switzerland')
+#cnames = StringVar(value=countrynames)
+#populations = {'ar':41000000, 'au':21179211, 'be':10584534, 'br':185971537, \
+#        'ca':33148682, 'cn':1323128240, 'dk':5457415, 'fi':5302000, 'fr':64102140, 'gr':11147000, \
+#        'in':1131043000, 'it':59206382, 'jp':127718000, 'mx':106535000, 'nl':16402414, \
+#        'no':4738085, 'es':45116894, 'se':9174082, 'ch':7508700}
+
+## Names of the gifts we can send
+#gifts = { 'card':'Greeting card', 'flowers':'Flowers', 'nastygram':'Nastygram'}
+
+## State variables
+#gift = StringVar()
+#sentmsg = StringVar()
+#statusmsg = StringVar()
+
+## Called when the selection in the listbox changes; figure out
+## which country is currently selected, and then lookup its country
+## code, and from that, its population.  Update the status message
+## with the new population.  As well, clear the message about the
+## gift being sent, so it doesn't stick around after we start doing
+## other things.
+#def showPopulation(*args):
+#    idxs = lbox.curselection()
+#    if len(idxs)==1:
+#        idx = int(idxs[0])
+#        code = countrycodes[idx]
+#        name = countrynames[idx]
+#        popn = populations[code]
+#        statusmsg.set("The population of %s (%s) is %d" % (name, code, popn))
+#    sentmsg.set('')
+
+## Called when the user double clicks an item in the listbox, presses
+## the "Send Gift" button, or presses the Return key.  In case the selected
+## item is scrolled out of view, make sure it is visible.
+##
+## Figure out which country is selected, which gift is selected with the 
+## radiobuttons, "send the gift", and provide feedback that it was sent.
+#def sendGift(*args):
+#    idxs = lbox.curselection()
+#    if len(idxs)==1:
+#        idx = int(idxs[0])
+#        lbox.see(idx)
+#        name = countrynames[idx]
+#        # Gift sending left as an exercise to the reader
+#        sentmsg.set("Sent %s to leader of %s" % (gifts[gift.get()], name))
+
+## Create and grid the outer content frame
+#c = ttk.Frame(root, padding=(5, 5, 12, 0))
+#c.grid(column=0, row=0, sticky=(N,W,E,S))
+#root.grid_columnconfigure(0, weight=1)
+#root.grid_rowconfigure(0,weight=1)
+
+## Create the different widgets; note the variables that many
+## of them are bound to, as well as the button callback.
+## Note we're using the StringVar() 'cnames', constructed from 'countrynames'
+#lbox = Listbox(c, listvariable=cnames, height=5)
+#lbl = ttk.Label(c, text="Send to country's leader:")
+#g1 = ttk.Radiobutton(c, text=gifts['card'], variable=gift, value='card')
+#g2 = ttk.Radiobutton(c, text=gifts['flowers'], variable=gift, value='flowers')
+#g3 = ttk.Radiobutton(c, text=gifts['nastygram'], variable=gift, value='nastygram')
+#send = ttk.Button(c, text='Send Gift', command=sendGift, default='active')
+#sentlbl = ttk.Label(c, textvariable=sentmsg, anchor='center')
+#status = ttk.Label(c, textvariable=statusmsg, anchor=W)
+
+## Grid all the widgets
+#lbox.grid(column=0, row=0, rowspan=6, sticky=(N,S,E,W))
+#lbl.grid(column=1, row=0, padx=10, pady=5)
+#g1.grid(column=1, row=1, sticky=W, padx=20)
+#g2.grid(column=1, row=2, sticky=W, padx=20)
+#g3.grid(column=1, row=3, sticky=W, padx=20)
+#send.grid(column=2, row=4, sticky=E)
+#sentlbl.grid(column=1, row=5, columnspan=2, sticky=N, pady=5, padx=5)
+#status.grid(column=0, row=6, columnspan=2, sticky=(W,E))
+#c.grid_columnconfigure(0, weight=1)
+#c.grid_rowconfigure(5, weight=1)
+
+## Set event bindings for when the selection in the listbox changes,
+## when the user double clicks the list, and when they hit the Return key
+#lbox.bind('<<ListboxSelect>>', showPopulation)
+#lbox.bind('<Double-1>', sendGift)
+#root.bind('<Return>', sendGift)
+
+## Colorize alternating lines of the listbox
+#for i in range(0,len(countrynames),2):
+#    lbox.itemconfigure(i, background='#f0f0ff')
+
+## Set the starting state of the interface, including selecting the
+## default gift to send, and clearing the messages.  Select the first
+## country in the list; because the <<ListboxSelect>> event is only
+## generated when the user makes a change, we explicitly call showPopulation.
+#gift.set('card')
+#sentmsg.set('')
+#statusmsg.set('')
+#lbox.selection_set(0)
+#showPopulation()
+
+#root.mainloop()
+
+
+
+
 import datetime
 
 from tkinter import *
@@ -48,22 +160,81 @@ class App(object):
     def __init__(self):
         self._root = Tk()
         self._root.title('DashBoard Tool')
-        self._root.columnconfigure(0, weight=1)
-        self._root.rowconfigure(0, weight=1)
+        #self._root.columnconfigure(0, weight=1)
+        #self._root.rowconfigure(0, weight=1)
         
         window = Frame(self._root)
         window.grid(row=0, column=0, sticky=(N,S,E,W))
+
+        Button(window, text="Вверх", command=self.command_up).grid(row=0, column=0, sticky=(N,S,E,W), padx=5, pady=5)
+        Button(window, text="Вниз", command=self.command_down).grid(row=1, column=0, sticky=(N,S,E,W), padx=5, pady=5)
+        self._selListBox = Listbox(window)
+        self._selListBox.grid(row=0, column=1, rowspan=2)
+        Button(window, text="Включить", command=self.command_include).grid(row=0, column=2, sticky=(N,S,E,W), padx=5, pady=5)
+        Button(window, text="Выключить", command=self.command_exclude).grid(row=1, column=2, sticky=(N,S,E,W), padx=5, pady=5)
+        self._srcListBox = Listbox(window)
+        self._srcListBox.grid(row=0, column=3, rowspan=2)
+
+        window.columnconfigure(1, weight=1)
+        window.columnconfigure(3, weight=1)
+        window.rowconfigure(0, weight=1)
         window.rowconfigure(1, weight=1)
-        window.columnconfigure(0, weight=1)
 
-        for r in range(0, 10):
-            Checkbutton(window, text="Demo Text - " + str(r)).grid(row=r, column=0)
+        self._selList = []
+        self._srcList = ["Main", "Time", "Alarm", "Voice", "YandexNews", "OpenWeatherMap", "WunderGround", "Calendar", "Swap", "Watcher"]
 
+        for item in self._selList:  self._selListBox.insert("end", item)
+        self._selListBox.selection_set(0)
+        for item in self._srcList:  self._srcListBox.insert("end", item)
+        self._srcListBox.selection_set(0)
 
         self._root.bind('<Key-Escape>', lambda e: self._root.destroy())
         #self._root.resizable(False, False)
         self._root.geometry("+0+0")
         self._root.mainloop()
+    
+    def command_up(self):
+        selection = self._selListBox.curselection() 
+        if not selection: return
+        if selection[0] == 0: return
+        item = self._selList.pop(selection[0])
+        self._selList.insert(selection[0] - 1, item)
+        self.update_selected_list(selection[0] - 1)
+
+
+    def command_down(self):
+        selection = self._selListBox.curselection() 
+        if not selection: return
+        if selection[0] == len(self._selList) - 1: return
+        item = self._selList.pop(selection[0])
+        self._selList.insert(selection[0] + 1, item)
+        self.update_selected_list(selection[0] + 1)
+
+    def command_include(self):
+        selection = self._srcListBox.curselection() 
+        if not selection: return
+        name = self._srcListBox.get(selection[0])
+        if name in self._selList: return
+
+        self._selList.append(name)
+        self.update_selected_list()
+        pass
+
+    def command_exclude(self):
+        selection = self._srcListBox.curselection() 
+        if not selection: return
+        name = self._srcListBox.get(selection[0])
+        if name not in self._selList: return
+
+        self._selList.remove(name)
+        self.update_selected_list()
+        pass
+
+    def update_selected_list(self, selection = None):
+        self._selListBox.delete(0, 'end')
+        for item in self._selList:  self._selListBox.insert("end", item)
+        if selection:
+            self._selListBox.selection_set(selection)
 
 def main():            
     App()
@@ -86,3 +257,10 @@ if __name__ == "__main__":
     #print((date2 - currentDate).seconds)
     #print(3600*24)
 
+    #list = [ "Main", "Time", "Alarm", "Voice", "YandexNews", "OpenWeatherMap", "WunderGround", "Calendar", "Swap", "Watcher"]
+    #print(list)
+    #item = list.pop(1)
+    #print(item)
+    #print(list)
+    #list.insert(0, item)
+    #print(list)
