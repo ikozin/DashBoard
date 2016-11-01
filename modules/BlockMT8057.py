4268b4001454712df7d78ecedf978b35eae43011
Ilya <ikozin@yandex.ru>
2016-11-01 13:17:01 +0300

    fix

--- modules/BlockMT8057.py
+++ modules/BlockMT8057.py
@@ -3,6 +3,8 @@ import configparser
 import pygame
 import pygame.locals
 import usb.core
+import usb.util
+import threading
 
 from exceptions import ExceptionFormat, ExceptionNotFound
 from modules.BlockBase import BlockBase
@@ -10,19 +12,12 @@ from modules.BlockBase import BlockBase
 
 class BlockMT8057(BlockBase):
 	"""description of class"""
-	VID = 0x04d9
-	PID = 0xa052
-
-	magic_buf   = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
-	ctmp        = [0x84, 0x47, 0x56, 0xd6, 0x07, 0x93, 0x93, 0x56]
-
 	def __init__(self, logger, setting):
 		"""Initializes (declare internal variables)"""
 		super(BlockMT8057, self).__init__(logger, setting)
 		self._green = None
 		self._yellow = None
-		self._had_driver = False
-		self._dev = None
+		self._t_mt8057 = None
 
 
 	def init(self, fileName, isOnline, modList):
@@ -40,41 +35,82 @@ class BlockMT8057(BlockBase):
 		if self._green is None:  raise ExceptionNotFound(section.name, "Green")
 		if self._yellow is None: raise ExceptionNotFound(section.name, "Yellow")
 
-		self._had_driver = False
-		self._dev = usb.core.find(idVendor=self.VID, idProduct=self.PID)
-		if self._dev is None:   raise ValueError("Device not found")
-		if self._dev.is_kernel_driver_active(0):
-			self._dev.detach_kernel_driver(0)
-			self._had_driver = True
-		self._dev.set_configuration()
-		self._dev.ctrl_transfer(0x21, 0x09, 0x0200, 0x00, [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96])
+		self._t_mt8057 = mt8057()
+		self._t_mt8057.start()
 
 		self.updateInfo(isOnline)
 
 
-	def proccedEvent(self, event, isOnline):
-		pass
-
-
-	def updateInfo(self, isOnline):
-		try:
-			pass
-		except Exception as ex:
-			self._logger.exception(ex)
-
-
 	def updateDisplay(self, isOnline, screen, size, foreColor, backColor, current_time):
 		try:
-			pass
+			(co2, temp) = self._t_mt8057.get_data()
+			print("CO2", co2, "temp ", "{:.1f}".format(temp));
 		except Exception as ex:
 			self._logger.exception(ex)
 
 
 	def done(self):
 		""" """
-		usb.util.release_interface(self._dev, 0)
-		if self._had_driver:
-			self._dev.attach_kernel_driver(0)
+		self._t_mt8057.stop()
+		self._t_mt8057.join()
+
+
+
+
+class mt8057(threading.Thread):
+	VID = 0x04d9
+	PID = 0xa052
+	RW_TIMEOUT = 5000
+	REQUEST_TYPE_SEND = usb.util.build_request_type(usb.util.CTRL_OUT, usb.util.CTRL_TYPE_CLASS, usb.util.CTRL_RECIPIENT_INTERFACE)
+	REQ_HID_SET_REPORT = 0x09
+	HID_REPORT_TYPE_FEATURE = 0x03 << 8
+
+	magic_buf = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
+	ctmp    = [0x84, 0x47, 0x56, 0xd6, 0x07, 0x93, 0x93, 0x56]
+
+	def __init__(self):
+		threading.Thread.__init__(self, name="mt")
+		self._event_stop = threading.Event()
+		self._lock = threading.Lock()
+		self._temperature  = None
+		self._concentration = None
+		self._had_driver = False
+		self._dev = usb.core.find(idVendor=self.VID, idProduct=self.PID)
+
+		if self._dev is None:
+			raise ValueError("Device not found")
+
+		if self._dev.is_kernel_driver_active(0):
+			self._dev.detach_kernel_driver(0)
+			self._had_driver = True
+
+		self._dev.set_configuration()
+		#print(self._dev)
+		self._ep = self._dev[0][(0,0)][0]
+
+
+	def stop(self):
+		self._event_stop.set()
+
+
+	def run(self):
+		self._dev.ctrl_transfer(self.REQUEST_TYPE_SEND, self.REQ_HID_SET_REPORT, self.HID_REPORT_TYPE_FEATURE, 0x00, self.magic_buf, self.RW_TIMEOUT)
+		while not self._event_stop.is_set():
+			data = self._read()
+			#print(data)
+			self._parse(data)
+		self._release()
+
+
+	def get_data(self):
+		self._lock.acquire()
+		value = (self._concentration, self._temperature)
+		self._lock.release()
+		return value
+
+
+	def _read(self):
+		return self._dev.read(self._ep, 8, self.RW_TIMEOUT)
 
 
 	def _decode(self, data):
@@ -92,3 +128,30 @@ class BlockMT8057(BlockBase):
 			result.append((0x100 + phase3[i] - self.ctmp[i]) & 0xff)
 
 		return result
+
+
+	def _parse(self, data):
+		item = self._decode(data)
+		r0 = item[0]
+		r1 = item[1]
+		r2 = item[2]
+		r3 = item[3]
+		checksum = (r0 + r1 + r2) & 0xff
+		if (checksum == r3 and item[4] == 0x0d):
+			w = (r1 << 8) + r2
+			if (r0 == 0x42): # Ambient Temperature
+				w = w * 0.0625 - 273.15
+				self._lock.acquire()
+				self._temperature  = w
+				self._lock.release()
+			elif (r0 == 0x50): # Relative Concentration of CO2
+				self._lock.acquire()
+				self._concentration = w
+				self._lock.release()
+			else:
+				pass
+
+	def _release(self):
+		usb.util.release_interface(self._dev, 0)
+		if self._had_driver:
+			self._dev.attach_kernel_driver(0)
--- test.py
+++ test.py
@@ -1,80 +1,34 @@
 import time
 import datetime
+import threading
 
+class mt(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self, name="mt")
+        self.event_stop = threading.Event()
+
+    def stop(self):
+        self.event_stop.set()
+
+
+    def run(self):
+        while not self.event_stop.is_set():
+            print("run")
+            time.sleep(2)
 
 if __name__ == "__main__":
-
-    currentTime = datetime.datetime.now()
-
-    text = "Московское время {:%H:%M}".format(currentTime)
-    print(text)
-
-    #startTime = datetime.datetime.strptime("9:00:00", "%H:%M:%S")
-    #stopTime = datetime.datetime.strptime("20:00:00", "%H:%M:%S")
-
-    #t1 = currentTime - startTime
-    #t2 = currentTime - stopTime
-    #print(t1.seconds)
-    #print(t2.seconds)
-
-    #dtime = datetime.datetime.now()
-    #ans_time = time.mktime(dtime.timetuple())
-    #print(ans_time)
-
-    #print()
-    #print(24*60*60)
-    #print()
-
-    #startTime = datetime.datetime.strptime("14:59:00", "%H:%M:%S")
-    #print((currentTime.time() > startTime.time()))
-    #startTime = datetime.datetime.strptime("15:00:00", "%H:%M:%S")
-    #print((currentTime.time() > startTime.time()))
-    #startTime = datetime.datetime.strptime("15:01:00", "%H:%M:%S")
-    #print((currentTime.time() > startTime.time()))
-    #startTime = datetime.datetime.strptime("15:02:00", "%H:%M:%S")
-    #print((currentTime.time() > startTime.time()))
-    #startTime = datetime.datetime.strptime("15:03:00", "%H:%M:%S")
-    #print((currentTime.time() > startTime.time()))
-    #startTime = datetime.datetime.strptime("15:04:00", "%H:%M:%S")
-    #print((currentTime.time() > startTime.time()))
-
-    #print()
-    #print(24*60*60)
-    #print()
-
-    #startTime = datetime.datetime.strptime("14:59:00", "%H:%M:%S")
-    #print((startTime.time() > currentTime.time()))
-    #startTime = datetime.datetime.strptime("15:00:00", "%H:%M:%S")
-    #print((startTime.time() > currentTime.time()))
-    #startTime = datetime.datetime.strptime("15:01:00", "%H:%M:%S")
-    #print((startTime.time() > currentTime.time()))
-    #startTime = datetime.datetime.strptime("15:02:00", "%H:%M:%S")
-    #print((startTime.time() > currentTime.time()))
-    #startTime = datetime.datetime.strptime("15:02:00", "%H:%M:%S")
-    #print((startTime.time() > currentTime.time()))
-    #startTime = datetime.datetime.strptime("15:04:00", "%H:%M:%S")
-    #print((startTime.time() > currentTime.time()))
-
-
-    #print()
-    #print(startTime.time())
-    #print()
-    #print(currentTime.time())
-    #print()
-
-    ##print(ans_time)
-
-    ##print((currentDate - date1).seconds)
-    ##print((date1 - currentDate).seconds)
-    ##print(3600*24)
-    ##print((currentDate - date2).seconds)
-    ##print((date2 - currentDate).seconds)
-    ##print(3600*24)
-
-    ##list = [ "Main", "Time", "Alarm", "Voice", "YandexNews", "OpenWeatherMap", "WunderGround", "Calendar", "Swap", "Watcher"]
-    ##print(list)
-    ##item = list.pop(1)
-    ##print(item)
-    ##print(list)
-    ##list.insert(0, item)
-    ##print(list)
+    t = mt()
+    t.start()
+    time.sleep(5)
+    print(t.isAlive())
+    time.sleep(5)
+    print(t.isAlive())
+    time.sleep(5)
+    print(t.isAlive())
+    time.sleep(5)
+    print(t.isAlive())
+    t.stop()
+    print(t.isAlive())
+    t.join()
+    print(t.isAlive())
