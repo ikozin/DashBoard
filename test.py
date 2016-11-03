7e32aeeebbce5c1c630e3d276140de2efb647589
Ilya <ikozin@yandex.ru>
2016-11-03 11:20:42 +0300

    fix

--- test.py
+++ test.py
@@ -8,12 +8,13 @@ class mt(threading.Thread):
         threading.Thread.__init__(self, name="mt")
         self.event_stop = threading.Event()
 
+
     def stop(self):
         self.event_stop.set()
 
 
     def run(self):
-        while not self.event_stop.is_set():
+        while not self.event_stop.isSet():
             print("run")
             time.sleep(2)
 
