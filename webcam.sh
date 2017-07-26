4deea6ab0d73e546646d46c6a66bba68468491d9
ikozin <ikozin@IKOZIN-PC>
2017-07-26 17:06:41 +0300

    fix

--- webcam.sh
+++ webcam.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d_%H:%M:%S")
 fswebcam -r 960x720 /home/pi/webcam/$DATE.jpg
-cp /home/pi/webcam/$DATE.jpg /mnt/yandex.disk/webcam/$DATE.jpg
+cp /home/pi/webcam/$DATE.jpg /mnt/yandex.disk/webcam/
+
