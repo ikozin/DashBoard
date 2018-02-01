8e2d1dbf49d3731f23705c07ba76e7c6dfb031a2
Ilya <ikozin@yandex.ru>
2018-02-01 11:32:26 +0300

    test

--- test.py
+++ test.py
@@ -1,127 +1,101 @@
+import datetime
+import os
 import sys
-import time
-import threading
-import usb.core
-import usb.util
+import subprocess
+import configparser
+import locale
+import pygame
+import pygame.locals
 
-class mt8057(threading.Thread):
-    VID = 0x04d9
-    PID = 0xa052
-    RW_TIMEOUT = 0
-    REQUEST_TYPE_SEND = usb.util.build_request_type(
-        usb.util.CTRL_OUT,
-        usb.util.CTRL_TYPE_CLASS,
-        usb.util.CTRL_RECIPIENT_INTERFACE)
-    REQ_HID_SET_REPORT = 0x09
-    HID_REPORT_TYPE_FEATURE = 0x03 << 8
+FPS = 30
+# WAIT_TIME = 40
+BLOCK_TIME_DISPLAY_FORMAT = "{:%H:%M}"
 
-    magic_buf = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
-    ctmp = [0x84, 0x47, 0x56, 0xd6, 0x07, 0x93, 0x93, 0x56]
+
+class Mainboard:
 
     def __init__(self):
-        threading.Thread.__init__(self, name="mt")
-        self._event_stop = threading.Event()
-        self._lock = threading.Lock()
-        self._temperature = None
-        self._concentration = None
-        self._had_driver = False
-        self._dev = usb.core.find(idVendor=self.VID, idProduct=self.PID)
+        """ """
+        self._modules = []
+        self._size = None
+        self._screen = None
 
-        if self._dev is None:
-            raise ValueError("Device not found")
+        # Based on "Python GUI in Linux frame buffer"
+        # http://www.karoltomala.com/blog/?p=679
+        # Check which frame buffer drivers are available
+        # Start with fbcon since directfb hangs with composite output
+        drivers = ["fbcon", "directfb", "svgalib", "xvfb", "x11", "windib", "directx"]
+        found = False
+        for driver in drivers:
+            # Make sure that SDL_VIDEODRIVER is set
+            if not os.getenv("SDL_VIDEODRIVER"):
+                os.putenv("SDL_VIDEODRIVER", driver)
+            try:
+                pygame.display.init()
+            except pygame.error:
+                print("Driver: {0} failed.".format(driver))
+                continue
+            found = True
+            break
 
-        if self._dev.is_kernel_driver_active(0):
-            self._dev.detach_kernel_driver(0)
-            self._had_driver = True
+        if not found:
+            raise Exception("No suitable video driver found!")
+        self._size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+        self._screen = pygame.display.set_mode(self._size, pygame.FULLSCREEN | pygame.HWSURFACE)
+        print("Framebuffer size: {0}".format(self._size))
 
-        self._dev.set_configuration()
-        # print(self._dev)
-        self._ep = self._dev[0][(0, 0)][0]
+        # Инициализируем шрифты
+        pygame.font.init()
+        # Инициализируем музыкальный модуль
+        pygame.mixer.init()
+        pygame.mixer.music.set_volume(0.0)
 
-    def stop(self):
-        self._event_stop.set()
+        # Выключаем курсор
+        pygame.mouse.set_visible(False)
 
-    def run(self):
-        self._dev.ctrl_transfer(
-            self.REQUEST_TYPE_SEND,
-            self.REQ_HID_SET_REPORT,
-            self.HID_REPORT_TYPE_FEATURE,
-            0x00, self.magic_buf,
-            self.RW_TIMEOUT)
-        self._event_stop.clear()
-        while not self._event_stop.is_set():
-            data = self._read()
-            self._parse(data)
-            # time.sleep(0.1)
-        self._release()
+        self._font = pygame.font.SysFont("Helvetica", 256, True, False)
 
-    def get_data(self):
-        self._lock.acquire()
-        value = (self._concentration, self._temperature)
-        self._lock.release()
-        return value
 
-    def _read(self):
-        return self._dev.read(self._ep, 8, self.RW_TIMEOUT)
+    def __del__(self):
+        """Destructor to make sure pygame shuts down, etc."""
+        pygame.mixer.quit()
+        pygame.display.quit()
 
-    def _decode(self, data):
-        shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
-        phase1 = []
-        phase2 = []
-        phase3 = []
-        result = []
 
-        for i in range(8):
-            phase1.append(data[shuffle[i]])
-            phase2.append(phase1[i] ^ self.magic_buf[i])
-        for i in range(8):
-            phase3.append(((phase2[i] >> 3) | (phase2[(i - 1 + 8) % 8] << 5)) & 0xff)
-            result.append((0x100 + phase3[i] - self.ctmp[i]) & 0xff)
+    def proccedEvent(self, events):
+        for event in events:
+            if (event.type == pygame.locals.QUIT):
+                return 0
+            if (event.type == pygame.locals.KEYDOWN and event.key == pygame.locals.K_ESCAPE):
+                return 0
+        return 1
 
-        return result
+    def loop(self):
+        clock = pygame.time.Clock()
+        while (self.proccedEvent(pygame.event.get())):
 
-    def _parse(self, data):
-        item = self._decode(data)
-        r0 = item[0]
-        r1 = item[1]
-        r2 = item[2]
-        r3 = item[3]
-        checksum = (r0 + r1 + r2) & 0xff
-        if (checksum == r3 and item[4] == 0x0d):
-            w = (r1 << 8) + r2
-            if (r0 == 0x42):  # Ambient Temperature
-                w = w * 0.0625 - 273.15
-                self._lock.acquire()
-                self._temperature = w
-                self._lock.release()
-            elif (r0 == 0x50):  # Relative Concentration of CO2
-                self._lock.acquire()
-                self._concentration = w
-                self._lock.release()
-            else:
-                pass
+            self._screen.fill((0, 0, 0))
 
-    def _release(self):
-        usb.util.release_interface(self._dev, 0)
-        if self._had_driver:
-            self._dev.attach_kernel_driver(0)
+            time = datetime.datetime.now()
+            text = BLOCK_TIME_DISPLAY_FORMAT.format(time)
+            sz = self._font.size(text)
+            x = (self._size[0] - sz[0]) >> 1
+            y = (self._size[1] - sz[1]) >> 1
+            surf = self._font.render(text, True, (64, 64, 64), (0, 0, 0))
+            self._screen.blit(surf, (x, y))
+
+
+            pygame.display.update()
+            # pygame.display.flip()
+            # pygame.time.delay(WAIT_TIME)
+            clock.tick(FPS)
+
+        pygame.quit()
+
 
 if __name__ == "__main__":
-    t_mt8057 = mt8057()
-    t_mt8057.start()
 
-    time.sleep(1)
-    (valueCO2, valueTemp) = t_mt8057.get_data()
-    print(valueCO2, valueTemp)
-
-    time.sleep(1)
-    (valueCO2, valueTemp) = t_mt8057.get_data()
-    print(valueCO2, valueTemp)
-
-    time.sleep(5)
-    (valueCO2, valueTemp) = t_mt8057.get_data()
-    print(valueCO2, valueTemp)
-
-    t_mt8057.stop()
-    t_mt8057.join()
-    print("Done!")
+    print(pygame.version.ver)
+    app = Mainboard()
+    app.loop()
+    sys.exit(0)
