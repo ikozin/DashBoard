37ec873c1ee9da3e0931e89ced2e63d30ff1946e
Ilya Kozin <ikozin@yandex.ru>
2019-05-06 16:22:46 +0300

    update

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-import lirc
+#data = "1,2,3".split(",")
+#print(data)
+#print ('%s.' % ', '.join(data))
+#data = ('1', '2', '3')
+#print ('%s.' % ', '.join(data))
 
-sockid = lirc.init("dashboard")
-print(lirc.nextcode())
-lirc.deinit()
+
+print()
+
+def func(*args):
+
+    print(args, end=" ")
+    print(len(args))
+
+    step = int(args[0]) if len(args) == 1 else 1
+    print(step)
+
+
+func(-2)
