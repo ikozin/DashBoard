a8d4054014e462de8d4488b7341fce26ab5d81c1
Ilya Kozin <ikozin@yandex.ru>
2020-02-22 12:00:06 +0300

    bme280

--- ext/bme280_manager.py
+++ ext/bme280_manager.py
@@ -2,7 +2,7 @@ from typing import Dict, Tuple
 from configparser import ConfigParser
 from tkinter import IntVar, StringVar, Entry, LabelFrame, Label, Spinbox, N, S, E, W, RIGHT
 from ext.base_manager import BaseManager
-from ext.modal_dialog import FontChooserFrame, XYFrame
+from ext.modal_dialog import DisplayTextFrame
 
 
 class Bme280Manager(BaseManager):
@@ -13,9 +13,6 @@ class Bme280Manager(BaseManager):
         super(Bme280Manager, self).__init__(root, text="Настройки BME280, Параметры: "
                                                        "Температура={0}, Влажность={1}, Давление={2}")
         self._address_value = IntVar()
-        self._temperature_text_value = StringVar()
-        self._humidity_text_value = StringVar()
-        self._pressure_text_value = StringVar()
         self._format_text_value = StringVar()
 
         content = LabelFrame(self, text="Основные настрйки")
@@ -30,38 +27,14 @@ class Bme280Manager(BaseManager):
         entr = Entry(content, width=60, textvariable=self._format_text_value)
         entr.grid(row=1, column=1, padx=2, pady=2, sticky=(N, S, E, W))
 
-        content = LabelFrame(self, text="Температура")
-        content.grid(row=1, column=0, padx=2, pady=2, sticky=(N, S, E, W))
+        self._temperature = DisplayTextFrame(self, "Температура", "Temperature")
+        self._temperature.grid(row=1, column=0, padx=2, pady=2, sticky=(N, S, E, W))
 
-        lbl = Label(content, justify=RIGHT, text="Текст")
-        lbl.grid(row=0, column=0, padx=2, pady=2, sticky=(N, S, E))
-        entr = Entry(content, width=68, textvariable=self._temperature_text_value)
-        entr.grid(row=0, column=1, padx=2, pady=2, sticky=(N, S, E, W))
-        self._pos_temperature = XYFrame(content, "Расположение", "Расположение (X)", "Расположение (Y)")
-        self._pos_temperature.grid(row=1, column=0, columnspan=2, padx=2, pady=2, sticky=(N, S, E, W))
+        self._humidity = DisplayTextFrame(self, "Влажность", "Humidity")
+        self._humidity.grid(row=2, column=0, padx=2, pady=2, sticky=(N, S, E, W))
 
-        content = LabelFrame(self, text="Влажность")
-        content.grid(row=2, column=0, padx=2, pady=2, sticky=(N, S, E, W))
-
-        lbl = Label(content, justify=RIGHT, text="Текст")
-        lbl.grid(row=0, column=0, padx=2, pady=2, sticky=(N, S, E))
-        entr = Entry(content, width=68, textvariable=self._humidity_text_value)
-        entr.grid(row=0, column=1, padx=2, pady=2, sticky=(N, S, E, W))
-        self._pos_humidity = XYFrame(content, "Расположение", "Расположение (X)", "Расположение (Y)")
-        self._pos_humidity.grid(row=1, column=0, columnspan=2, padx=2, pady=2, sticky=(N, S, E, W))
-
-        content = LabelFrame(self, text="Давление")
-        content.grid(row=3, column=0, padx=2, pady=2, sticky=(N, S, E, W))
-
-        lbl = Label(content, justify=RIGHT, text="Текст")
-        lbl.grid(row=0, column=0, padx=2, pady=2, sticky=(N, S, E))
-        entr = Entry(content, width=68, textvariable=self._pressure_text_value)
-        entr.grid(row=0, column=1, padx=2, pady=2, sticky=(N, S, E, W))
-        self._pos_pressure = XYFrame(content, "Расположение", "Расположение (X)", "Расположение (Y)")
-        self._pos_pressure.grid(row=1, column=0, columnspan=2, padx=2, pady=2, sticky=(N, S, E, W))
-
-        self._font = FontChooserFrame(self, "Параметры шрифта")
-        self._font.grid(row=4, column=0, padx=2, pady=2, sticky=(N, S, E, W))
+        self._pressure = DisplayTextFrame(self, "Давление", "Pressure")
+        self._pressure.grid(row=3, column=0, padx=2, pady=2, sticky=(N, S, E, W))
 
     def load(self, config: ConfigParser, module_list: Dict[str, BaseManager]) -> None:
         if not isinstance(config, ConfigParser):
@@ -70,21 +43,10 @@ class Bme280Manager(BaseManager):
             config.add_section("BME280Block")
         section = config["BME280Block"]
         self._address_value.set(section.getint("Address", fallback=118))
-        font_name = section.get("FontName", fallback="Helvetica")
-        font_size = section.getint("FontSize", fallback=150)
-        is_bold = section.getboolean("FontBold", fallback=True)
-        is_italic = section.getboolean("FontItalic", fallback=False)
-        self._font.load(font_name, font_size, is_bold, is_italic)
-        self._temperature_text_value.set(section.get("TemperatureText", fallback=""))
-        self._humidity_text_value.set(section.get("HumidityText", fallback=""))
-        self._pressure_text_value.set(section.get("PressureText", fallback=""))
-        (pos_x, pos_y) = self._get_tuple(section.get("TemperaturePos", fallback="(0, 0)"))
-        self._pos_temperature.load(pos_x, pos_y)
-        (pos_x, pos_y) = self._get_tuple(section.get("HumidityPos", fallback="(0, 0)"))
-        self._pos_humidity.load(pos_x, pos_y)
-        (pos_x, pos_y) = self._get_tuple(section.get("PressurePos", fallback="(0, 0)"))
-        self._pos_pressure.load(pos_x, pos_y)
         self._format_text_value.set(section.get("FormatText", fallback=""))
+        self._temperature.load(section)
+        self._humidity.load(section)
+        self._pressure.load(section)
 
     def save(self, config: ConfigParser) -> None:
         if not isinstance(config, ConfigParser):
@@ -93,21 +55,10 @@ class Bme280Manager(BaseManager):
             config.add_section("BME280Block")
         section = config["BME280Block"]
         section["Address"] = str(self._address_value.get())
-        (font_name, font_size, is_bold, is_italic) = self._font.get_result()
-        section["FontName"] = font_name
-        section["FontSize"] = str(font_size)
-        section["FontBold"] = str(is_bold)
-        section["FontItalic"] = str(is_italic)
-        section["TemperatureText"] = self._temperature_text_value.get()
-        section["HumidityText"] = self._humidity_text_value.get()
-        section["PressureText"] = self._pressure_text_value.get()
-        (pos_x, pos_y) = self._pos_temperature.get_result()
-        section["TemperaturePos"] = "({0},{1})".format(pos_x, pos_y)
-        (pos_x, pos_y) = self._pos_humidity.get_result()
-        section["HumidityPos"] = "({0},{1})".format(pos_x, pos_y)
-        (pos_x, pos_y) = self._pos_pressure.get_result()
-        section["PressurePos"] = "({0},{1})".format(pos_x, pos_y)
         section["FormatText"] = self._format_text_value.get()
+        self._temperature.save(section)
+        self._humidity.save(section)
+        self._pressure.save(section)
 
     def _get_tuple(self, value: str) -> Tuple[int, ...]:
         """  Конвертирует строку '0, 0, 0' в кортеж (0, 0, 0) """
--- modules/block_bme280.py
+++ modules/block_bme280.py
@@ -22,13 +22,22 @@ class BlockBme280(BlockBase):
         self._temperature = None
         self._pressure = None
         self._humidity = None
-        self._font = None
+        self._temperature_font = None
+        self._humidity_font = None
+        self._pressure_font = None
         self._temperature_text = ""
         self._humidity_text = ""
         self._pressure_text = ""
         self._temperature_pos = None
         self._humidity_pos = None
         self._pressure_pos = None
+        self._temperature_align_x = ""
+        self._temperature_align_y = ""
+        self._humidity_align_x = ""
+        self._humidity_align_y = ""
+        self._pressure_align_x = ""
+        self._pressure_align_y = ""
+
         self._format = ""
 
     def init(self, mod_list: Dict[str, BlockBase]) -> None:
@@ -37,42 +46,78 @@ class BlockBme280(BlockBase):
         section = self._setting.configuration["BME280Block"]
 
         self._address = section.getint("Address")
-        font_name = section.get("FontName")
-        font_size = section.getint("FontSize")
-        is_bold = section.getboolean("FontBold")
-        is_italic = section.getboolean("FontItalic")
-        self._temperature_text = section.get("TemperatureText")
-        self._humidity_text = section.get("HumidityText")
-        self._pressure_text = section.get("PressureText")
-        self._temperature_pos = self._get_tuple(section.get("TemperaturePos"))
-        self._humidity_pos = self._get_tuple(section.get("HumidityPos"))
-        self._pressure_pos = self._get_tuple(section.get("PressurePos"))
         self._format = section.get("FormatText")
 
+        self._temperature_text = section.get("TemperatureText")
+        temperature_font_name = section.get("TemperatureFontName")
+        temperature_font_size = section.getint("TemperatureFontSize")
+        temperature_is_bold = section.getboolean("TemperatureFontBold")
+        temperature_is_italic = section.getboolean("TemperatureFontItalic")
+        self._temperature_pos = self._get_tuple(section.get("TemperaturePos"))
+        self._temperature_align_x = section.get("TemperatureAlignX")
+        self._temperature_align_y = section.get("TemperatureAlignY")
+
+        self._humidity_text = section.get("HumidityText")
+        humidity_font_name = section.get("HumidityFontName")
+        humidity_font_size = section.getint("HumidityFontSize")
+        humidity_is_bold = section.getboolean("HumidityFontBold")
+        humidity_is_italic = section.getboolean("HumidityFontItalic")
+        self._humidity_pos = self._get_tuple(section.get("HumidityPos"))
+        self._humidity_align_x = section.get("HumidityAlignX")
+        self._humidity_align_y = section.get("HumidityAlignY")
+
+        self._pressure_text = section.get("PressureText")
+        pressure_font_name = section.get("PressureFontName")
+        pressure_font_size = section.getint("PressureFontSize")
+        pressure_is_bold = section.getboolean("PressureFontBold")
+        pressure_is_italic = section.getboolean("PressureFontItalic")
+        self._pressure_pos = self._get_tuple(section.get("PressurePos"))
+        self._pressure_align_x = section.get("PressureAlignX")
+        self._pressure_align_y = section.get("PressureAlignY")
+        
         if self._address is None:
             raise ExceptionNotFound(section.name, "Address")
-        if font_name is None:
-            raise ExceptionNotFound(section.name, "FontName")
-        if font_size is None:
-            raise ExceptionNotFound(section.name, "FontSize")
-        if is_bold is None:
-            raise ExceptionNotFound(section.name, "FontBold")
-        if is_italic is None:
-            raise ExceptionNotFound(section.name, "FontItalic")
-        if self._temperature_text is None:
-            raise ExceptionNotFound(section.name, "TemperatureText")
-        if self._humidity_text is None:
-            raise ExceptionNotFound(section.name, "HumidityText")
-        if self._pressure_text is None:
-            raise ExceptionNotFound(section.name, "PressureText")
-        if self._temperature_pos is None:
-            raise ExceptionNotFound(section.name, "TemperaturePos")
-        if self._humidity_pos is None:
-            raise ExceptionNotFound(section.name, "HumidityPos")
-        if self._pressure_pos is None:
-            raise ExceptionNotFound(section.name, "PressurePos")
         if self._format is None:
             raise ExceptionNotFound(section.name, "FormatText")
+
+        if self._temperature_text is None:
+            raise ExceptionNotFound(section.name, "TemperatureText")
+        if temperature_font_name is None:
+            raise ExceptionNotFound(section.name, "TemperatureFontName")
+        if temperature_font_size is None:
+            raise ExceptionNotFound(section.name, "TemperatureFontSize")
+        if temperature_is_bold is None:
+            raise ExceptionNotFound(section.name, "TemperatureFontBold")
+        if temperature_is_italic is None:
+            raise ExceptionNotFound(section.name, "TemperatureFontItalic")
+        if self._temperature_pos is None:
+            raise ExceptionNotFound(section.name, "TemperaturePos")
+
+        if self._humidity_text is None:
+            raise ExceptionNotFound(section.name, "HumidityText")
+        if humidity_font_name is None:
+            raise ExceptionNotFound(section.name, "HumidityFontName")
+        if humidity_font_size is None:
+            raise ExceptionNotFound(section.name, "HumidityFontSize")
+        if humidity_is_bold is None:
+            raise ExceptionNotFound(section.name, "HumidityFontBold")
+        if humidity_is_italic is None:
+            raise ExceptionNotFound(section.name, "HumidityFontItalic")
+        if self._humidity_pos is None:
+            raise ExceptionNotFound(section.name, "HumidityPos")
+
+        if self._pressure_text is None:
+            raise ExceptionNotFound(section.name, "PressureText")
+        if pressure_font_name is None:
+            raise ExceptionNotFound(section.name, "PressureFontName")
+        if pressure_font_size is None:
+            raise ExceptionNotFound(section.name, "PressureFontSize")
+        if pressure_is_bold is None:
+            raise ExceptionNotFound(section.name, "PressureFontBold")
+        if pressure_is_italic is None:
+            raise ExceptionNotFound(section.name, "PressureFontItalic")
+        if self._pressure_pos is None:
+            raise ExceptionNotFound(section.name, "PressurePos")
 
         if len(self._temperature_pos) != 2:
             raise ExceptionFormat(section.name, "TemperaturePos")
@@ -81,7 +126,9 @@ class BlockBme280(BlockBase):
         if len(self._pressure_pos) != 2:
             raise ExceptionFormat(section.name, "PressurePos")
 
-        self._font = pygame.font.SysFont(font_name, font_size, is_bold, is_italic)
+        self._temperature_font = pygame.font.SysFont(temperature_font_name, temperature_font_size, temperature_is_bold, temperature_is_italic)
+        self._humidity_font = pygame.font.SysFont(humidity_font_name, humidity_font_size, humidity_is_bold, humidity_is_italic)
+        self._pressure_font = pygame.font.SysFont(pressure_font_name, pressure_font_size, pressure_is_bold, pressure_is_italic)
 
         self._device = self._hal(self._logger, self._address)
         self.update_info(True)
@@ -98,16 +145,19 @@ class BlockBme280(BlockBase):
                 return
             if len(self._temperature_text) > 0:
                 text = self._temperature_text.format(self._temperature, self._humidity, self._pressure)
-                surf = self._font.render(text, True, fore_color, back_color)
-                screen.blit(surf, self._temperature_pos)
+                text_size = self._temperature_font.size(text)
+                surf = self._temperature_font.render(text, True, fore_color, back_color)
+                screen.blit(surf, self.calc_position(text_size, self._temperature_pos, self._temperature_align_x, self._temperature_align_y))
             if len(self._humidity_text) > 0:
                 text = self._humidity_text.format(self._temperature, self._humidity, self._pressure)
-                surf = self._font.render(text, True, fore_color, back_color)
-                screen.blit(surf, self._humidity_pos)
+                text_size = self._humidity_font.size(text)
+                surf = self._humidity_font.render(text, True, fore_color, back_color)
+                screen.blit(surf, self.calc_position(text_size, self._humidity_pos, self._humidity_align_x, self._humidity_align_y))
             if len(self._pressure_text) > 0:
                 text = self._pressure_text.format(self._temperature, self._humidity, self._pressure)
-                surf = self._font.render(text, True, fore_color, back_color)
-                screen.blit(surf, self._pressure_pos)
+                text_size = self._pressure_font.size(text)
+                surf = self._pressure_font.render(text, True, fore_color, back_color)
+                screen.blit(surf, self.calc_position(text_size, self._pressure_pos, self._pressure_align_x, self._pressure_align_y))
         except Exception as ex:
             self._logger.exception(ex)
 
