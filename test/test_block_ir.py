c28cd368b0ac700f71238e75635d152593495a78
ikozin <ikozin@yandex.ru>
2020-05-11 10:11:41 +0300

    test

--- test/test_block_ir.py
+++ test/test_block_ir.py
@@ -25,105 +25,63 @@ def test_block_ir(logger, mocker):
 
 
 @pytest.mark.block_ir
-def test_block_ir_exec(logger, mocker):
+def test_block_ir_exec_NO_KEY(logger, mocker):
     config = _get_setting(None)
+
     block = BlockIR(logger, config, Lirc_Stub)
     assert block is not None
     assert isinstance(block, BlockBase)
 
     mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
     inst = mock_block.return_value
-    inst.execute.return_value = None
-    block.init({"Swap": inst, "Voice": inst})
-
-    block.execute("KEY")
+    block.init({"Block": inst})
+    block.execute("NO_KEY")
     inst.execute.assert_not_called()
 
 
 @pytest.mark.block_ir
-def test_block_ir_exec_KEY_0(logger, mocker, monkeypatch):
+def test_block_ir_exec_KEY(logger, mocker):
     config = _get_setting(None)
 
-    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY_0")
+    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY")
     block = BlockIR(logger, config, Lirc_Stub)
     assert block is not None
     assert isinstance(block, BlockBase)
 
     mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
     inst = mock_block.return_value
-    inst.execute.return_value = None
-    block.init({"Swap": None, "Voice": inst})
-    block.execute()  # "key_0": "Voice",
+    block.init({"Block": inst})
+    block.execute()
     inst.execute.assert_called_once_with()
 
 
 @pytest.mark.block_ir
-def test_block_ir_exec_KEY_1(logger, mocker, monkeypatch):
+def test_block_ir_exec_KEY_PARAM(logger, mocker):
     config = _get_setting(None)
 
-    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY_1")
+    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY_PARAM")
     block = BlockIR(logger, config, Lirc_Stub)
     assert block is not None
     assert isinstance(block, BlockBase)
 
     mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
     inst = mock_block.return_value
-    inst.execute.return_value = None
-    block.init({"Swap": None, "Voice": inst})
-    block.execute()  # "key_1": "Voice,parameter",
+    block.init({"Block": inst})
+    block.execute()
     inst.execute.assert_called_once_with("parameter")
 
 
-@pytest.mark.block_ir
-def test_block_ir_exec_KEY_CHANNELDOWN(logger, mocker, monkeypatch):
-    config = _get_setting(None)
-
-    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY_CHANNELDOWN")
-    block = BlockIR(logger, config, Lirc_Stub)
-    assert block is not None
-    assert isinstance(block, BlockBase)
-
-    mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
-    inst = mock_block.return_value
-    inst.execute.return_value = None
-    block.init({"Swap": inst, "Voice": None})
-    block.execute()  # "key_channeldown": "Swap,-1",
-    inst.execute.assert_called_once_with("-1")
-
-
-@pytest.mark.block_ir
-def test_block_ir_exec_KEY_CHANNELUP(logger, mocker, monkeypatch):
-    config = _get_setting(None)
-
-    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY_CHANNELUP")
-    block = BlockIR(logger, config, Lirc_Stub)
-    assert block is not None
-    assert isinstance(block, BlockBase)
-
-    mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
-    inst = mock_block.return_value
-    inst.execute.return_value = None
-    block.init({"Swap": inst, "Voice": None})
-    block.execute()  # "key_channelup": "Swap,1"
-    inst.execute.assert_called_once_with("1")
-
-
-def _get_setting(name):
-    params = {
-        "key_0": "Voice,",
-        "key_1": "Voice,parameter",
-        "key_channeldown": "Swap,-1",
-        "key_channelup": "Swap,1"
-    }
+def _get_setting(params):
+    if not params:
+        params = {
+            "KEY": "Block,",
+            "KEY_PARAM": "Block,parameter",
+        }
     config = Setting()
     config.configuration.add_section(SECTION_NAME)
-    if name == "":
-        return config
     section = config.configuration[SECTION_NAME]
     for key, value in params.items():
         section[key] = value.__str__()
-        if key == name:
-            break
     return config
 
 
