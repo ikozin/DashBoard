b180280b19325a87c14879fd1f10f352abcd5264
Ilya Kozin <ikozin@yandex.ru>
2020-05-11 09:29:59 +0300

    test

--- test/test_block_ir.py
+++ test/test_block_ir.py
@@ -25,44 +25,6 @@ def test_block_ir(logger, mocker):
 
 
 @pytest.mark.block_ir
-def test_block_ir_execute(logger, mocker):
-    config = _get_setting(None)
-    block = BlockIR(logger, config, Lirc_Stub)
-    assert block is not None
-    assert isinstance(block, BlockBase)
-
-    mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
-    inst = mock_block.return_value
-    inst.execute.return_value = None
-    block.init({"Swap": inst, "Voice": inst})
-
-    inst.execute.reset_mock()
-    block.execute()  # "key_0": "Voice",
-    block.execute()  # "key_1": "Voice",
-    block.execute()  # "key_2": "Voice",
-    block.execute()  # "key_3": "Voice",
-    block.execute()  # "key_4": "Voice",
-    block.execute()  # "key_5": "Voice",
-    block.execute()  # "key_6": "Voice",
-    block.execute()  # "key_7": "Voice",
-    block.execute()  # "key_8": "Voice",
-    block.execute()  # "key_9": "Voice",
-    assert inst.execute.call_count == 10
-
-    inst.execute.reset_mock()
-    block.execute()  # "key_channeldown": "Swap,-1",
-    inst.execute.assert_called_once_with("-1")
-
-    inst.execute.reset_mock()
-    block.execute()  # "key_channelup": "Swap,1"
-    inst.execute.assert_called_once_with("1")
-
-    inst.execute.reset_mock()
-    block.execute("KEY")
-    inst.execute.assert_not_called()
-
-
-@pytest.mark.block_ir
 def test_block_ir_exec(logger, mocker):
     config = _get_setting(None)
     block = BlockIR(logger, config, Lirc_Stub)
@@ -73,22 +35,83 @@ def test_block_ir_exec(logger, mocker):
     inst = mock_block.return_value
     inst.execute.return_value = None
     block.init({"Swap": inst, "Voice": inst})
+
+    block.execute("KEY")
+    inst.execute.assert_not_called()
+
+
+@pytest.mark.block_ir
+def test_block_ir_exec_KEY_0(logger, mocker, monkeypatch):
+    config = _get_setting(None)
+    
+    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY_0")
+    block = BlockIR(logger, config, Lirc_Stub)
+    assert block is not None
+    assert isinstance(block, BlockBase)
+
+    mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
+    inst = mock_block.return_value
+    inst.execute.return_value = None
+    block.init({"Swap": None, "Voice": inst})
     block.execute()  # "key_0": "Voice",
-    inst.execute.assert_called_once()
+    inst.execute.assert_called_once_with()
+
+
+@pytest.mark.block_ir
+def test_block_ir_exec_KEY_1(logger, mocker, monkeypatch):
+    config = _get_setting(None)
+    
+    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY_1")
+    block = BlockIR(logger, config, Lirc_Stub)
+    assert block is not None
+    assert isinstance(block, BlockBase)
+
+    mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
+    inst = mock_block.return_value
+    inst.execute.return_value = None
+    block.init({"Swap": None, "Voice": inst})
+    block.execute()  # "key_1": "Voice,parameter",
+    inst.execute.assert_called_once_with("parameter")
+
+
+@pytest.mark.block_ir
+def test_block_ir_exec_KEY_CHANNELDOWN(logger, mocker, monkeypatch):
+    config = _get_setting(None)
+    
+    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY_CHANNELDOWN")
+    block = BlockIR(logger, config, Lirc_Stub)
+    assert block is not None
+    assert isinstance(block, BlockBase)
+
+    mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
+    inst = mock_block.return_value
+    inst.execute.return_value = None
+    block.init({"Swap": inst, "Voice": None})
+    block.execute()  # "key_channeldown": "Swap,-1",
+    inst.execute.assert_called_once_with("-1")
+
+
+@pytest.mark.block_ir
+def test_block_ir_exec_KEY_CHANNELUP(logger, mocker, monkeypatch):
+    config = _get_setting(None)
+    
+    mocker.patch.object(Lirc_Stub, 'getCode', return_value="KEY_CHANNELUP")
+    block = BlockIR(logger, config, Lirc_Stub)
+    assert block is not None
+    assert isinstance(block, BlockBase)
+
+    mock_block = mocker.patch('modules.BlockBase.BlockBase', spec=True)
+    inst = mock_block.return_value
+    inst.execute.return_value = None
+    block.init({"Swap": inst, "Voice": None})
+    block.execute()  # "key_channelup": "Swap,1"
+    inst.execute.assert_called_once_with("1")
 
 
 def _get_setting(name):
     params = {
         "key_0": "Voice,",
-        "key_1": "Voice,",
-        "key_2": "Voice,",
-        "key_3": "Voice,",
-        "key_4": "Voice,",
-        "key_5": "Voice,",
-        "key_6": "Voice,",
-        "key_7": "Voice,",
-        "key_8": "Voice,",
-        "key_9": "Voice,",
+        "key_1": "Voice,parameter",
         "key_channeldown": "Swap,-1",
         "key_channelup": "Swap,1"
     }
@@ -110,23 +133,6 @@ class Lirc_Stub(Lirc_Base):
     def __init__(self, logger: Logger):
         """Initializes (declare internal variables)"""
         super(Lirc_Stub, self).__init__(logger)
-        self._key_list = [
-            "KEY_0",
-            "KEY_1",
-            "KEY_2",
-            "KEY_3",
-            "KEY_4",
-            "KEY_5",
-            "KEY_6",
-            "KEY_7",
-            "KEY_8",
-            "KEY_9",
-            "KEY_CHANNELDOWN",
-            "KEY_CHANNELUP"]
-        self._index = 0
 
     def getCode(self, code: str = None) -> str:
-        code = self._key_list[self._index]
-        self._index = self._index + 1
-        self._index = self._index % len(self._key_list)
-        return code
+        pass
