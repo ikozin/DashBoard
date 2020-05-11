397f7ffb4ac6949a16b2e743ebebb211cacda98d
ikozin <ikozin@yandex.ru>
2020-05-11 13:01:17 +0300

    test

--- test/test_block_mt8057.py
+++ test/test_block_mt8057.py
@@ -23,29 +23,145 @@ def test_block_mt8057(logger):
         block.init({})
 
 
+@pytest.mark.block_mt8057
+def test_init_format_warn(logger, mocker):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "", "Warn")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_crit(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "Warn", "Crit")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_warn_color(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "Crit", "WarnColor")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_crit_color(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "WarnColor", "CritColor")
+
+@pytest.mark.block_mt8057
+def test_init_format_co2_text(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "CritColor", "CO2Text")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_co2_font_name(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "CO2Text", "CO2FontName")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_co2_font_size(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "CO2FontName", "CO2FontSize")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_co2_font_bold(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "CO2FontSize", "CO2FontBold")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_co2_font_italic(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "CO2FontBold", "CO2FontItalic")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_co2_pos(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "CO2FontItalic", "CO2Pos")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_co2_align_x(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "CO2Pos", "CO2AlignX")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_co2_align_y(logger):
+    mocker.patch("modules.block_mt8057.MT8057", spec=True)
+    check_property(logger, "CO2AlignX", "CO2AlignY")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_temp_text(logger):
+    check_property(logger, "CO2AlignY", "TempText")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_temp_font_name(logger):
+    check_property(logger, "TempText", "TempFontName")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_temp_font_size(logger):
+    check_property(logger, "TempFontName", "TempFontSize")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_temp_font_bold(logger):
+    check_property(logger, "TempFontSize", "TempFontBold")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_temp_font_italic(logger):
+    check_property(logger, "TempFontBold", "TempFontItalic")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_temp_pos(logger):
+    check_property(logger, "TempFontItalic", "TempPos")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_temp_align_x(logger):
+    check_property(logger, "TempPos", "TempAlignX")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_temp_align_y(logger):
+    check_property(logger, "TempAlignX", "TempAlignY")
+
+
+@pytest.mark.block_mt8057
+def test_init_format_format_text(logger):
+    check_property(logger, "TempAlignY", "FormatText")
+
+
 def _get_setting(name):
     params = {
         "Warn": 800,
         "Crit": 1200,
         "WarnColor": (255, 255, 0),
         "CritColor": (255, 63, 63),
-        "FormatText": "Концентрация CO2: {0}.Температура: {1:+.1f}",
-        "Co2Text": "Концентрация CO2: {0}",
-        "Co2FontName": "Helvetica",
-        "Co2FontSize": 100,
-        "Co2FontBold": True,
-        "Co2FontItalic": False,
-        "Co2Pos": (380, 32),
-        "Co2AlignX": "Left",
-        "Co2AlignY": "Top",
-        "TempText": "Температура: {1:+.1f}°",
+        "CO2Text": "Концентрация CO2: {0}",
+        "CO2FontName": "Helvetica",
+        "CO2FontSize": 140,
+        "CO2FontBold": True,
+        "CO2FontItalic": False,
+        "CO2Pos": (960, 200),
+        "CO2AlignX": "Center",
+        "CO2AlignY": "Center",
+        "TempText": "",
         "TempFontName": "Helvetica",
-        "TempFontSize": 100,
+        "TempFontSize": 140,
         "TempFontBold": True,
         "TempFontItalic": False,
-        "TempPos": (460, 180),
+        "TempPos": (0, 0),
         "TempAlignX": "Left",
-        "TempAlignY": "Top"
+        "TempAlignY": "Top",
+        "FormatText": "Концентрация CO2: {0}.Температура: {1:+.1f}."
     }
     config = Setting()
     config.configuration.add_section(SECTION_NAME)
