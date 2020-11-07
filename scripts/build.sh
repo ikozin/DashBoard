7e3eee778e9a67cd63a36d657fba83160f866717
ikozin <ikozin@yandex.ru>
2020-11-07 10:41:54 +0300

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,4 +16,4 @@ sed -i -e 's/<OPENWEATHERMAP_KEY>/'${OPENWEATHER_KEY}'/g' setting_win.ini
 sed -i -e 's/<YANDEXVOICE_KEY>/'${YANDEX_VOICE_KEY}'/g' setting_win.ini
 sed -i -e 's/<WUNDERGROUND_KEY>/'${WUNDERGROUND_KEY}'/g' setting_win.ini
 # zip
-zip build -R ext modules music weather "*.py" "*.ini" "*.sh" "*.cmd" "*.txt" "*.mp3" "*.png" ".gif" -x test/*
+zip build -R ext modules music weather "*.py" "*.ini" "*.sh" "*.cmd" "*.txt" "*.mp3" "*.png" "*.gif" -x test/*
